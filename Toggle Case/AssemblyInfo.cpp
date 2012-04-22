1c4c7eb08ba42b278c395fa831b15f812f720338
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-22 03:41:04 +0300

    Added copyright notices and license.

--- Toggle Case/AssemblyInfo.cpp
+++ Toggle Case/AssemblyInfo.cpp
@@ -14,9 +14,9 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTitleAttribute("Toggle Case")];
 [assembly:AssemblyDescriptionAttribute("")];
 [assembly:AssemblyConfigurationAttribute("")];
-[assembly:AssemblyCompanyAttribute("The Dark Side")];
+[assembly:AssemblyCompanyAttribute("-")];
 [assembly:AssemblyProductAttribute("Toggle Case")];
-[assembly:AssemblyCopyrightAttribute("Copyright (c) The Dark Side 2008")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c) Vaptistis Anogeianakis 2008, 2012")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
